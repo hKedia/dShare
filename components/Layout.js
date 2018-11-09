e8922688c123a3690ded97833b511c08ac737b0c
Harsh Kedia <harshkedia05@gmail.com>
2018-11-09 15:44:54 +0100

    1. Adding Loader 2. Updating Factory Contract Address 3. Adding nprogress css file

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import Head from "next/head";
 import { Container } from "semantic-ui-react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Router from "next/router";
+import NProgress from "nprogress";
 
-export default props => {
-  return (
-    <div>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"
-        />
-      </Head>
-      <Header />
-      <Container style={{ margin: "5em 5em" }}>{props.children}</Container>
-      <Footer />
-    </div>
-  );
-};
+Router.events.on("routeChangeStart", url => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
+class Layout extends Component {
+  render() {
+    return (
+      <div>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"
+          />
+          <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+        </Head>
+        <Header />
+        <Container style={{ margin: "5em 5em" }}>
+          {this.props.children}
+        </Container>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Layout;
--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import FileFactory from "./build/FileFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(FileFactory.interface),
-  "0xc46Fc425dF353Fb7270053e31a9C8D1cfBDC21A9"
+  "0x67E103943a35Dc478594381ffc667CCCe6d162E2"
 );
 
 export default instance;
