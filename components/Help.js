a723cb56752bd05ccd035257d6546e395b77ce64
Harsh Kedia <harshkedia05@gmail.com>
2018-11-26 16:10:39 +0100

    updating help pages

--- components/Help.js
+++ components/Help.js
@@ -11,31 +11,73 @@ class Help extends Component {
         content = (
           <Segment basic size="big">
             <p>
-              This page lists the files uploaded by you as well as the files
-              which are shared with you by other users.
+              This page lists the files. On the left column you see the files
+              uploaded by you and on the right column you see the files shared
+              with you by others.
+            </p>
+
+            <p>
+              Initially, the UI shows the contract address of the deployed file
+              but as a file is viewed, the file name is fetched and saved in the
+              local storage.
+            </p>
+
+            <p>
+              You can use the top menu panel to navigate to different parts of
+              the application.
+            </p>
+
+            <p>
+              The bottom panel shows the current Ethereum account and the
+              network the application is connected to.
             </p>
           </Segment>
         );
         break;
 
       case "/sharedFiles":
-        content = <p>Content for Shared Files</p>;
+        content = (
+          <Segment basic size="big">
+            <p>
+              This page shows the files which you have shared with other users.
+            </p>
+          </Segment>
+        );
         break;
 
       case "/archivedFiles":
-        content = <p>Content for Archived Files</p>;
+        content = (
+          <Segment basic size="big">
+            <p>This page shows the archived files.</p>
+          </Segment>
+        );
         break;
 
       case "/proof":
-        content = <p>Content for Proof</p>;
+        content = (
+          <Segment basic size="big">
+            <p>
+              This page can be used to download the timestamping proof for any
+              file.
+            </p>
+          </Segment>
+        );
         break;
 
       case "/upload":
-        content = <p>Content for Upload</p>;
+        content = (
+          <Segment basic size="big">
+            <p>Content for Upload</p>
+          </Segment>
+        );
         break;
 
       case "/view":
-        content = <p>Content for File View page</p>;
+        content = (
+          <Segment basic size="big">
+            <p>Content for File View page</p>
+          </Segment>
+        );
         break;
 
       default:
